e97018c3d7fddace617b9716772437503632c269
Felipe Braga <contato@fbsis.com>
2024-04-13 21:20:54 -0300

    new function to stop and destroy

--- lxc.sh
+++ lxc.sh
@@ -37,6 +37,33 @@ create_container() {
     echo "LXC container $container_name created successfully."
 }
 
+# Function to stop a running LXC container
+stop_container() {
+    read -p "Enter the name of the container you want to stop: " container_name
+    
+    if lxc info $container_name &> /dev/null; then
+        echo "Stopping the container $container_name"
+        lxc stop $container_name
+    else
+        echo "Container $container_name does not exist."
+    fi
+}
+
+# Function to destroy an LXC container
+destroy_container() {
+    read -p "Enter the name of the container you want to destroy: " container_name
+
+    if lxc info $container_name &> /dev/null; then
+        echo "Stopping the container $container_name"
+        lxc stop $container_name
+
+        echo "Destroying the container $container_name"
+        lxc delete --force $container_name
+    else
+        echo "Container $container_name does not exist."
+    fi
+}
+
 # Main logic for LXC functionality
 option=""
 
@@ -46,7 +73,9 @@ while true; do
     echo "1. List LXC containers"
     echo "2. Enter an LXC container by name"
     echo "3. Create a new LXC container"
-    echo "4. Exit"
+    echo "4. Stop a running LXC container"
+    echo "5. Destroy an LXC container"
+    echo "6. Exit"
 
     read -p "Choose an option: " option
 
@@ -54,7 +83,9 @@ while true; do
         1) list_containers;;
         2) enter_container;;
         3) create_container;;
-        4) echo "Exiting the LXC script."
+        4) stop_container;;
+        5) destroy_container;;
+        6) echo "Exiting the LXC script."
            break;;
         *) echo "Invalid option. Please try again."; continue;;
     esac
